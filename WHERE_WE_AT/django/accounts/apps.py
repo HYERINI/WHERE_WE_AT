08380a916e50f5e3a6f2f8df32c8768f2b361234
HYERINI <rinlee2001@naver.com>
2021-08-10 23:43:30 +0900

    map

--- WHERE_WE_AT/django/accounts/apps.py
+++ WHERE_WE_AT/django/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WhereweatConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'whereweat'
+    name = 'accounts'
